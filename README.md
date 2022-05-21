fa921e343e7e96aeadb8580b9d4da6075d1caa37
EDUARDOLSIMAO <eduardo_simao@hotmail.com>
2022-05-20 23:15:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-v52whh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-v52whh)
---
+++ index.js
@@ -0,0 +1,11 @@
+debugger
+let mostrarmensagem = false;
+if (mostrarmensagem == true) {
+  console.log('mensagem');
+}
+else {
+  let mensagemerro = 'Ops';
+  console.log(mensagemerro);
+}
+console.log('Olá Mundo')
+console.log('Novato iniciando na programação')
